67563cd2607c39fe8d1d1868e06487ab8c4121fe
Matesx <matteo.mougeot@gmail.com>
2017-03-17 09:19:00 +0100

    Création des activitees multijoueur

---
+++ app/src/main/java/com/example/matteo/challengento_mkj/Multijoueur/EnregistrementScenario.java
@@ -0,0 +1,15 @@
+package com.example.matteo.challengento_mkj.Multijoueur;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.matteo.challengento_mkj.R;
+
+public class EnregistrementScenario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enregistrement_scenario);
+    }
+}
--- app/src/main/java/com/example/matteo/challengento_mkj/Multijoueur/Score.java
+++ app/src/main/java/com/example/matteo/challengento_mkj/Multijoueur/Score.java
@@ -1,15 +1,15 @@
-package com.example.matteo.challengento_mkj;
+package com.example.matteo.challengento_mkj.Multijoueur;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.matteo.challengento_mkj.R;
+
+public class Score extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_score);
     }
 }
