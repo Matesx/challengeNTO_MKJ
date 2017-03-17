fd0134266cacff25d295fd92f76521707a1f63c3
Girox64 <kevingiraud64@gmail.com>
2017-03-17 12:57:21 +0100

    DétectionMouvement fait avec la liste de données passée à l'autre activité

--- app/src/main/java/com/example/matteo/challengento_mkj/Multijoueur/EnregistrementScenario.java
+++ app/src/main/java/com/example/matteo/challengento_mkj/Multijoueur/EnregistrementScenario.java
@@ -5,6 +5,8 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Handler;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -18,9 +20,13 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class EnregistrementScenario extends AppCompatActivity implements SensorEventListener {
+    private TextView textView;
     private SensorManager sensorManager;
     private Sensor magnetic;
     private Sensor accelerometer;
+
+    private Timer timerPrelevDonnees;
+    private Timer timerJeu;
 
     // Attribut de la classe pour calculer  l'orientation et l'accelerometre
     private float[] acceleromterVector=new float[3];
@@ -29,17 +35,27 @@ public class EnregistrementScenario extends AppCompatActivity implements SensorE
     private float[] values=new float[3];
 
     private List<Donnees> listDonnees;
+    private int compteur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enregistrement_scenario);
 
+        // Instancier le SensorManager
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        // Instantiate the magnetic sensor and its max range
+        magnetic = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        // Instantiate the accelerometer
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        textView = (TextView) findViewById(R.id.textView);
         listDonnees = new ArrayList<Donnees>();
 
+        compteur=6;
+        textView.setText(""+compteur);
 
-
-        final Timer timerPrelevDonnees = new Timer();
+        timerPrelevDonnees = new Timer();
         final TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
@@ -48,30 +64,37 @@ public class EnregistrementScenario extends AppCompatActivity implements SensorE
                     public void run() {
                         Donnees donnees = new Donnees(acceleromterVector, values[1], values[2]);
                         listDonnees.add(donnees);
+
                     }
                 });
             }
         };
 
-        timerPrelevDonnees.scheduleAtFixedRate(timerTask, 0, Long.valueOf(200));
+        timerPrelevDonnees.schedule(timerTask, 0, Long.valueOf(200));
 
-        final Timer timerJeu = new Timer();
+
+        timerJeu = new Timer();
         timerJeu.schedule(new TimerTask() {
             @Override
             public void run() {
                 runOnUiThread(new Runnable(){
                     @Override
                     public void run(){
-                        timerPrelevDonnees.cancel();
-                        timerJeu.cancel();
+                        textView.setText(""+ --compteur);
 
-                        Intent intent = new Intent(EnregistrementScenario.this, LancementScenario.class);
-                        startActivity(intent);
+                        if (compteur ==0) {
+                            timerPrelevDonnees.cancel();
+                            timerJeu.cancel();
+
+                            Intent intent = new Intent(EnregistrementScenario.this, LancementScenario.class);
+                            intent.putParcelableArrayListExtra("list", (ArrayList<Donnees>) listDonnees);
+                            startActivity(intent);
+                        }
                     }
                 });
 
             }
-        }, 5000, 5000);
+        }, 0, 1000);
     }
 
     @Override
@@ -108,5 +131,9 @@ public class EnregistrementScenario extends AppCompatActivity implements SensorE
         super.onDestroy();
         sensorManager.unregisterListener(this,accelerometer);
         sensorManager.unregisterListener(this,magnetic);
+        timerPrelevDonnees.cancel();
+        timerPrelevDonnees.purge();
+        timerJeu.cancel();
+        timerJeu.purge();
     }
 }
