f7dad4b8966efc63d50654fbcfe44d7825713be5
Matesx <matteo.mougeot@gmail.com>
2017-03-17 16:06:16 +0100

    IHM OK

--- app/src/main/java/com/example/matteo/challengento_mkj/Multijoueur/Score.java
+++ app/src/main/java/com/example/matteo/challengento_mkj/Multijoueur/Score.java
@@ -1,9 +1,14 @@
 package com.example.matteo.challengento_mkj.Multijoueur;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.matteo.challengento_mkj.Jouer;
+import com.example.matteo.challengento_mkj.MainActivity;
 import com.example.matteo.challengento_mkj.R;
 
 public class Score extends AppCompatActivity {
@@ -15,7 +20,27 @@ public class Score extends AppCompatActivity {
         setContentView(R.layout.activity_score);
 
         textView = (TextView) findViewById(R.id.textViewScore);
+        final Button buttonReplay = (Button) findViewById(R.id.buttonReplay);
+        final Button buttonMenu = (Button) findViewById(R.id.buttonMenu);
 
-        textView.setText(""+getIntent().getExtras().get("score"));
+        textView.setText("Votre Score : "+getIntent().getExtras().get("score"));
+
+        buttonReplay.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent1 = new Intent(Score.this, CreationScenario.class);
+                startActivity(intent1);
+                finish();
+
+            }
+        });
+
+        buttonMenu.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent1 = new Intent(Score.this, MainActivity.class);
+                startActivity(intent1);
+                finish();
+
+            }
+        });
     }
 }
